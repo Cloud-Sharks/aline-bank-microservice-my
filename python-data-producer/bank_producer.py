f1021317c933b8b3ef5d52f0773e0fb7a5cb69a8
Mark Yates <mark.yates@smoothstack.com>
2021-12-20 14:53:06 -0700

    cloud-108: interacts with api

--- python-data-producer/bank_producer.py
+++ python-data-producer/bank_producer.py
@@ -1,49 +1,27 @@
-import sqlalchemy as db
-from sqlalchemy import text
+import requests
 from faker import Faker
 
-# specify db config
-config = {
-    'host' : 'localhost',
-    'port' : 3307,
-    'user' : 'user',
-    'password' : 'pwd',
-    'database' : 'alinedb'
-}
-
-db_user = config.get('user')
-db_pwd = config.get('password')
-db_host = config.get('host')
-db_port = config.get('port')
-db_name = config.get('database')
-
-# specify connection string
-connection_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
-# connect to database
-engine = db.create_engine(connection_str)
-connection = engine.connect()
-
-def populate_bank(conn):
+def populate_bank(auth):
     fake = Faker()
-    # optional clear table entries
-    clear = True
-    if clear:
-        conn.execute(text('DELETE FROM bank'))
-        conn.execute(text('ALTER TABLE bank AUTO_INCREMENT = 1'))
+    register_url = 'http://localhost:8083/banks'
 
-    # create and insert X entries
-    num_entries = 10
-    for i in range(num_entries):
-        # define values to be inserted into user table
-        # id is bigInt auto-inc
-        address = fake.street_address() # varchar(255)
-        city = fake.city() # varchar(255)
-        routing_number = fake.numerify('#########') # varchar(9)
-        state = fake.state() # varchar(255)
-        zipcode = fake.zipcode() # varchar(255) nullable
+    bank_entries = 10
+    for i in range(bank_entries):
+        register_info = {
+            "routingNumber" : fake.numerify('#########'),
+            "address" : fake.street_address(),
+            "city" : fake.city(),
+            "state" : fake.state(),
+            "zipcode" : fake.numerify('#####')
+        }
+        reg_bank = requests.post(register_url, json=register_info, headers=auth)
+        # print(reg_bank.text)
 
-        # create and execute insert string
-        bank_ins = text("INSERT INTO bank (address, city, routing_number, state, zipcode) VALUES (:address, :city, :routing_number, :state, :zipcode)")
-        conn.execute(bank_ins, address=address, city=city, routing_number=routing_number, state=state, zipcode=zipcode)
-
-populate_bank(connection)
+# login_info = {
+#     'username' : 'adminUser',
+#     'password' : 'Password*8'
+# }
+# login_response = requests.post('http://localhost:8070/login', json=login_info)
+# bearer_token = login_response.headers['Authorization']
+# auth = {'Authorization' : bearer_token}
+# populate_bank(auth)
